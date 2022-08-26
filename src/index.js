6450ce2af3aa80a069a891b40698f2c8d6c6b113
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-26 14:51:52 +0200

    Added nested routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Link, Routes, Route, BrowserRouter as ReactRouter } from 'react-router-dom';
+import { Link, Routes, Route, BrowserRouter as ReactRouter, Outlet } from 'react-router-dom';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
@@ -11,20 +11,23 @@ appRoot.render(
         <nav>
             <ul>
                 <li><Link to='/hello'>Hello section</Link></li>
-                <li><Link to='/about'>About section</Link></li>
+                <li><Link to='/hello/orwa'>Hello orwa subsection</Link></li>
+                <li><Link to='/something-else'>Invalid section</Link></li>
             </ul>
         </nav>
 
         <Routes>
-
             <Route 
-                path='/hello'
-                element={<h2>The hello part had been accessed!</h2>} />
+                path='/hello' 
+                element={<div><h2>Hello section</h2><Outlet /></div>}>
+                    
+                <Route path=':name' element={<h3>Hello :name</h3>} />
+            </Route>
+
+            <Route
+                path='/*' 
+                element={<h2>404</h2>} />
             
-            <Route 
-                path='/about'
-                element={<h2>The about 🎂 section had been accessed!</h2>} />
-
         </Routes>
 
     </ReactRouter>
