662b724b909b692baf056f37917f61baf7ce4934
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-26 11:47:41 +0200

    Added a simple route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
 
+function Hello (props) {
+    return <h2>The hello part of the website had been accessed!</h2>;
+}
+
 appRoot.render(
-    <h1>My refresher app</h1>
+    <BrowserRouter>
+        <h1>My router app</h1>
+
+        <Routes>
+            <Route path='/hello' element={<Hello />} />
+        </Routes>
+    </BrowserRouter>
 );
