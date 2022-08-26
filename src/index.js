c9ce4e379a0a610e596421f15aa3ac07b2a369a1
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-26 15:13:27 +0200

    Added a home route

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Link, Routes, Route, BrowserRouter as ReactRouter, Outlet } from 'react-router-dom';
+import { Link, Routes, Route, BrowserRouter as ReactRouter, Outlet, useParams, Navigate } from 'react-router-dom';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
+
+function Hello(props) {
+    const { name } = useParams();
+
+    return <h2>Hello {name}</h2>;
+}
+
+function HelloWithIntention(props) {
+    const { name1, name2 } = useParams();
+
+    return <h2>{name1} is saying hello to {name2}</h2>;
+}
 
 appRoot.render(
     <ReactRouter>
         <h1>My router app</h1>
         <nav>
             <ul>
+                <li><Link to='/'>Home</Link></li>
                 <li><Link to='/hello'>Hello section</Link></li>
-                <li><Link to='/hello/orwa'>Hello orwa subsection</Link></li>
+                <li><Link to='/hello/Orwa'>Hello subsection</Link></li>
+                <li><Link to='/hello/Orwa/Peter'>Hello with intention subsection</Link></li>
+                <li><Link to='/hello/Orwa/Peter/Sergii'>Hello from the other side subsection</Link></li>
                 <li><Link to='/something-else'>Invalid section</Link></li>
             </ul>
         </nav>
 
         <Routes>
+
+            <Route
+                path='/'
+                element={<h1>Welcome to the homepage</h1>} />
+
             <Route 
                 path='/hello' 
-                element={<div><h2>Hello section</h2><Outlet /></div>}>
-                    
-                <Route path=':name' element={<h3>Hello :name</h3>} />
+                element={<div><h1>Hello section</h1><Outlet /></div>}>
+
+                <Route path=':name' element={<Hello />} />
+                <Route path=':name1/:name2' element={<HelloWithIntention />} />
+                <Route path='*' element={<h2>Hello from the other side.</h2>} />
             </Route>
 
             <Route
                 path='/*' 
-                element={<h2>404</h2>} />
+                element={<h1>404</h1>} />
             
         </Routes>
 
