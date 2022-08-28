070e85dbf008e06096d502217d8aa556cfc06179
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-28 10:28:14 +0200

    Added outlet context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Link, Routes, Route, BrowserRouter as ReactRouter, Outlet, useParams, Navigate } from 'react-router-dom';
+import { Link, Routes, Route, BrowserRouter as ReactRouter, Outlet, useParams, useOutletContext,  Navigate } from 'react-router-dom';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
 
 function Hello(props) {
     const { name } = useParams();
+    const { emoji } = useOutletContext();
 
-    return <h2>Hello {name}</h2>;
+    return <h2>Hello {name} {emoji} </h2>;
 }
 
 function HelloWithIntention(props) {
@@ -24,10 +25,10 @@ appRoot.render(
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/hello'>Hello section</Link></li>
-                <li><Link to='/hello/Orwa'>Hello subsection</Link></li>
-                <li><Link to='/hello/Orwa/Peter'>Hello with intention subsection</Link></li>
-                <li><Link to='/hello/Orwa/Peter/Sergii'>Hello from the other side subsection</Link></li>
-                <li><Link to='/something-else'>Invalid section</Link></li>
+                <li><Link to='/hello/Orwa' replace>Hello subsection</Link></li>
+                <li><Link to='/hello/Orwa/Peter' replace>Hello with intention subsection</Link></li>
+                <li><Link to='/hello/Orwa/Peter/Sergii' replace>Hello from the other side subsection</Link></li>
+                <li><Link to='/something-else' replace>Invalid section</Link></li>
             </ul>
         </nav>
 
@@ -35,11 +36,11 @@ appRoot.render(
 
             <Route
                 path='/'
-                element={<h1>Welcome to the homepage</h1>} />
+                element={<h1>Welcome to the homepage 😁</h1>} />
 
             <Route 
                 path='/hello' 
-                element={<div><h1>Hello section</h1><Outlet /></div>}>
+                element={<div><h1>Hello section</h1><Outlet context={{emoji: '🎶'}} /></div>}>
 
                 <Route path=':name' element={<Hello />} />
                 <Route path=':name1/:name2' element={<HelloWithIntention />} />
@@ -48,7 +49,7 @@ appRoot.render(
 
             <Route
                 path='/*' 
-                element={<h1>404</h1>} />
+                element={<Navigate to='/' />} />
             
         </Routes>
 
